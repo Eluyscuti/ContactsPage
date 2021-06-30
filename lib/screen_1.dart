0b918a34f9ae1c75050fb4d49d5b987865502336
BuildTools <unconfigured@null.spigotmc.org>
2021-06-29 17:21:24 -0700

    Worked on App bar

--- lib/screen_1.dart
+++ lib/screen_1.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Screen1 extends StatefulWidget {
@@ -9,9 +10,55 @@ class _Screen1State extends State<Screen1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //replace the body with the code for your page
+      appBar: AppBar(
+        title: Container(
+          width: 600,
+          height: 40,
+          margin: EdgeInsets.only(
+              left: 150
+          ),
+
+
+          child: TextField(
+              decoration: const InputDecoration(
+              hintText: 'Search',
+              border: const OutlineInputBorder(
+                borderRadius: const BorderRadius.all(const Radius.circular(20.0))
+              ),
+              
+              fillColor: Colors.white,
+              filled: true,
+
+            ),
+
+          ),
+
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(
+              right: 40,
+              top: 5,
+              bottom: 5,
+            ),
+
+            child: FlatButton(
+              onPressed: () {
+                ;
+              },
+              child: Icon(Icons.add),
+              color: Colors.white,
+              shape: CircleBorder(),
+
+
+            ),
+          )
+
+        ],
+        backgroundColor: Colors.blue[900],
+      ),
       body: Center(
-        child: Text('Page 1'),
+        child: Text('contacts'),
       ),
     );
   }
